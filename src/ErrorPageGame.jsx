1a82c3deec8db910fe56bef74df1a86f83c18ad1
Diegoo11 <ynoacamino@gmail.com>
2023-10-17 09:55:28 -0500

    add link button in error page

--- src/ErrorPageGame.jsx
+++ src/ErrorPageGame.jsx
@@ -1,16 +1,23 @@
+import { useNavigate } from 'react-router-dom';
+import { Button } from '@nextui-org/react';
 import error from './assets/error.svg';
 
 export default function ErrorPageGame() {
+  const navigate = useNavigate();
+
   return (
-    <div className="mb-8 w-40 md:w-60">
+    <div className="mb-8 w-40 md:w-60 flex flex-col justify-center items-center gap-6">
       <img
         className="bg-[#161e2b] rounded-md p-4"
         src={error}
         alt="error"
       />
       <span className="text-gray-300 font-semibold text-lg text-center flex items-center">
-        Play code not found, please create a new gamee
+        Play code not found, please create a new game
       </span>
+      <Button onPress={() => navigate('/play')}>
+        Home
+      </Button>
     </div>
   );
 }
